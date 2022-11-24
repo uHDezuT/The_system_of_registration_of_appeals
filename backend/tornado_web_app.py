b505d15a739335ad5c3b882836fdbb32f0266f11
Влад <vladon9961Git@gmail.com>
2022-11-24 17:57:50 +0300

    Заложил скелет web-сервера python(tornado)

---
+++ backend/tornado_web_app.py
@@ -0,0 +1,45 @@
+import asyncio
+
+import tornado.web
+
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("../frontend/register_form.html")
+
+    def post(self):
+        self.render("../frontend/register_form.html")
+        last_name = self.get_argument('last_name')
+        first_name = self.get_argument('first_name')
+        second_name = self.get_argument('second_name')
+        telephone = self.get_argument('telephone')
+        body = self.get_argument('body')
+        if last_name == '':
+            print('Заполните поле <Фамилия>')
+        elif first_name == '':
+            print('Заполните поле <Имя>')
+        elif second_name == '':
+            print('Заполните поле <Отчество>')
+        elif telephone == '':
+            print('Заполните поле <Телефон>')
+        elif body == '':
+            print('Напишите текст заявки')
+        else:
+            attribute = [last_name, first_name, second_name, telephone, body]
+            return print(attribute)
+
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+
+async def main():
+    app = make_app()
+    app.listen(8001)
+    await asyncio.Event().wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
