535116bacb8562e191fc1b86909f59fc8c908696
Влад <vladon9961Git@gmail.com>
2022-11-29 20:39:02 +0300

    подправил send_message.py. Кодирнул входящий словарь в json

--- rabbitmq/send_message.py
+++ rabbitmq/send_message.py
@@ -1,26 +1,32 @@
 import pika
+import json
 
 
 def send_message(attr: dict):  # добавление словаря в очередь
     connection = pika.BlockingConnection(pika.ConnectionParameters(
-        '127.0.0.1'))
+        host='localhost',
+        port=5672))
     channel = connection.channel()
     channel.queue_declare(queue='appeal')
     channel.basic_publish(exchange='',
                           routing_key='appeal',
-                          body=attr)
+                          body=json.dumps(attr))
     connection.close()
 
 
 def receive_message():
     connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host='127.0.0.1'))
+        host='localhost',
+        port=5672))
     channel = connection.channel()
 
     channel.queue_declare(queue='appeal')
 
-    body = channel.basic_get('appeal')
-    if body:
-        print(body)
-    else:
-        print('No message returned')
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % json.loads(body))
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(callback,
+                          queue='appeal')
+
+    channel.start_consuming()
